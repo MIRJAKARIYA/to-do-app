4cf2ca2a03445af26aeaacb98da15085173021bd
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-05-18 23:30:47 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
 
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+      </Routes>
     </div>
   );
 }
