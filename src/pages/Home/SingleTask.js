2d6fc9f5b4a3addae511f66f4c9548dc374ceb1c
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-05-19 04:11:41 +0600

    update task added

--- src/pages/Home/SingleTask.js
+++ src/pages/Home/SingleTask.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { AiFillDelete } from "react-icons/ai";
 
-const SingleTask = ({userTask,index,handleDelete}) => {
-    const {task, description,_id} = userTask;
+const SingleTask = ({ userTask, index, handleDelete, handleComplete }) => {
+  const { task, description, _id, completed } = userTask;
   return (
     <tr className="hover">
-      <th>{index+1}</th>
-      <td>{task}</td>
-      <td>{description}</td>
+      <th>{index + 1}</th>
+      <td className={`${completed && "line-through"}`}>{task}</td>
+      <td className={`${completed && "line-through"}`}>{description}</td>
       <td>
-          <button onClick={()=>handleDelete(_id)} className="btn btn-error btn-xs">delete</button>
+        <div className="flex items-center">
+          {!completed && (
+            <button
+              onClick={() => handleComplete(_id,task)}
+              className="btn mr-2 btn-success btn-xs"
+            >
+              complete
+            </button>
+          )}
+          <button
+            onClick={() => handleDelete(_id,task)}
+            className={`text-3xl text-red-700 ${completed && "ml-[85px]"}`}
+          >
+            <AiFillDelete></AiFillDelete>
+          </button>
+        </div>
       </td>
     </tr>
   );
--- src/pages/Home/ToDo.js
+++ src/pages/Home/ToDo.js
@@ -1,120 +1,159 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { toast, ToastContainer } from "react-toastify";
 import auth from "../../firebase.init";
 
 import SingleTask from "./SingleTask";
 
-
 const ToDo = () => {
-    const [reload, setReload] = useState(false)
-    const [tasks, setTasks] = useState([]);
-    const [user] = useAuthState(auth);
+  const [reload, setReload] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [user] = useAuthState(auth);
 
-    //get users to do data
+  //get users to do data
 
-    useEffect(()=>{
-        fetch(`http://localhost:5000/todos?email=${user?.email}`,{
-            method:'GET',
-            headers:{
-                authorization:`Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            setTasks(data);
-        })
-    },[reload,user])
+  useEffect(() => {
+    fetch(`http://localhost:5000/todos?email=${user?.email}`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(data);
+      });
+  }, [reload, user]);
 
-    //delete users to do data
+  //delete users to do data
 
-    const handleDelete = (id) =>{
-        fetch(`http://localhost:5000/todos/${id}`,{
-            method:'DELETE',
-            headers:{
-                authorization:`Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-        .then(res=> res.json())
-        .then(data=> {
-            console.log(data)
+  const handleDelete = (id,task) => {
+    fetch(`http://localhost:5000/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged === true) {
+            toast.warning(`${task} task deleted`);
             setReload(!reload);
-        })
-    }
+          }
+      });
+  };
 
-    //add users task
-    const handleAddTask = e =>{
-        e.preventDefault();
-        const task = e.target.taskName.value;
-        const description = e.target.taskDescription.value;
-        const email = user.email;
-        const taskDetail = {
-            task,description,email
+  //add users task
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    const task = e.target.taskName.value;
+    const description = e.target.taskDescription.value;
+    const email = user.email;
+    const taskDetail = {
+      task,
+      description,
+      email,
+    };
+    fetch("http://localhost:5000/todos", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify(taskDetail),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged === true) {
+          toast.success(`${task} task added successfully`);
+          setReload(!reload);
         }
-        fetch('http://localhost:5000/todos',{
-            method:'POST',
-            headers:{
-                'content-type':'application/json',
-                authorization:`Bearer ${localStorage.getItem('accessToken')}`
-            },
-            body:JSON.stringify(taskDetail)
-        })
-        .then(res=> res.json())
-        .then(data=> {
-            console.log(data)
-            setReload(!reload)
-        })
-
-    }
+      });
+      e.target.reset()
+  };
+  //update users task
+  const handleComplete = (id,task) => {
+    const isCompleted = { completed: true };
+    fetch(`http://localhost:5000/todos/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify(isCompleted),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged === true) {
+            toast.success(`${task} task added successfully`);
+            setReload(!reload);
+          }
+      });
+  };
 
   return (
-    <div className="mt-20">
-      <div className=" max-w-[500px] mx-auto">
-        <form onSubmit={handleAddTask}>
-          <div>
-            <p className="text-white text-lg font-semibold mb-2">
-              Task:
-            </p>
-            <input
-              type="text"
-              name="taskName"
-              placeholder="Enter Task"
-              className="input input-bordered input-warning w-full"
-            />
-          </div>
-          <div>
-            <p className="text-white text-lg font-semibold my-2">
-              Short Description:
-            </p>
-            <textarea
-            name="taskDescription"
-              className="textarea textarea-warning w-full"
-              placeholder="Enter Description"
-            ></textarea>
-          </div>
-          <button type="submit" className="btn btn-warning mx-auto font-bold block mt-5">ADD TASK</button>
-        </form>
+    <>
+      <div className="mt-20">
+        <div className=" max-w-[500px] mx-auto">
+          <form onSubmit={handleAddTask}>
+            <div>
+              <p className="text-white text-lg font-semibold mb-2">Task:</p>
+              <input
+                type="text"
+                name="taskName"
+                placeholder="Enter Task"
+                className="input input-bordered input-warning w-full"
+                required
+              />
+            </div>
+            <div>
+              <p className="text-white text-lg font-semibold my-2">
+                Short Description:
+              </p>
+              <textarea
+                name="taskDescription"
+                className="textarea textarea-warning w-full"
+                placeholder="Enter Description"
+                required
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-warning mx-auto font-bold block mt-5"
+            >
+              ADD TASK
+            </button>
+          </form>
+        </div>
+        <div className="overflow-x-auto mt-10">
+          <h1 className="text-center text-3xl mb-2 font-bold underline text-yellow-600">
+            Your Tasks
+          </h1>
+          <table className="table w-full max-w-[800px] mx-auto">
+            <thead>
+              <tr className="hover">
+                <th></th>
+                <th>Task</th>
+                <th>Description</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task, index) => (
+                <SingleTask
+                  key={task._id}
+                  userTask={task}
+                  index={index}
+                  handleDelete={handleDelete}
+                  handleComplete={handleComplete}
+                ></SingleTask>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div className="overflow-x-auto mt-10">
-        <h1 className="text-center text-3xl mb-2 font-bold underline text-yellow-600">
-          Your Tasks
-        </h1>
-        <table className="table w-full max-w-[800px] mx-auto">
-          <thead>
-            <tr className="hover">
-              <th></th>
-              <th>Task</th>
-              <th>Description</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-                tasks.map((task, index) => <SingleTask key={task._id} userTask={task} index={index} handleDelete={handleDelete}></SingleTask>)
-            }
-          </tbody>
-        </table>
-      </div>
-    </div>
+      <ToastContainer></ToastContainer>
+    </>
   );
 };
 
